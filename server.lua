ab17fd3d74174084e73ae2febea7286392066dc9
MrSociety404 <cstfabrice@gmail.com>
2021-06-15 15:07:00 +0200

    server

--- server.lua
+++ server.lua
@@ -1,15 +1,13 @@
 -- Sets server list convar
 SetConvarServerInfo('tags', 'LCDV_BIKES')
--- Setup for the version check
-updatePath = "/PLOKMJNB/FiveM-Civ-Car-Pack" -- your git user/repo path
 -- Console stuff
 Citizen.CreateThread(function()
-    if GetCurrentResourceName() ~= "LCDV_CARS" then
+    if GetCurrentResourceName() ~= "LCDV_BIKES" then
         print("-----------------------------------------------------")
         print("Please Dont Change the resource name to avoid errors.")
         print("-----------------------------------------------------")
     end
-    if GetCurrentResourceName() == 'LCDV_CARS' then
+    if GetCurrentResourceName() == 'LCDV_BIKES' then
         function checkVersion(err,responseText, headers)
             curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
                 print("-----------------------------------------------------------------------------------------------")
@@ -17,6 +15,4 @@ Citizen.CreateThread(function()
                 print("-----------------------------------------------------------------------------------------------")
         end
     end
-
-    PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/master/PLOKS_CARS/version", checkVersion, "GET")
 end)
