770512215c53c8f32cce1332e9f3013300c11c4d
MrSociety404 <cstfabrice@gmail.com>
2021-06-15 10:33:50 +0200

    add bike

--- vehicle_names.lua
+++ vehicle_names.lua
@@ -1,12 +1,15 @@
 Citizen.CreateThread(function()
 	--BMW
   AddTextEntry('bmws','BMW S1000 RR')
+	AddTextEntry('s1000rr','BMW S1000 RR 2020')
   --HONDA
 	AddTextEntry('450crf','Honda 450 CRF')
 	AddTextEntry('CBTWISTER','Honda 250 CB')
+	AddTextEntry('hcbr17','Honda CBR1000RR 2017')
   --KAWASAKI
 	AddTextEntry('nh2r','Kawasaki Ninja H2R')
 	AddTextEntry('kx450f','Kawasaki KX 450')
+	AddTextEntry('zx10r','Kawasaki ZX10 R')
 	--YAMAH
 	AddTextEntry('raptor700s','Yamaha Raptor 700S')
 	AddTextEntry('r6','Yamaha R6')
